f8d89cc30d5681288fac6618e2c29d80c6585cd4
Ian Harrison <itrharrison@gmail.com>
2023-12-05 11:51:15 +0000

    loop over maps

--- scripts/example_null_test.py
+++ scripts/example_null_test.py
@@ -11,7 +11,9 @@ from matplotlib import pyplot as plt
 
 plt.close('all')
 
-start_script = time.time()
+# start_script = time.time()
+
+data_dir = os.environ['ACTDIR']
 
 nside = 2048
 ell_min = 20
@@ -20,11 +22,22 @@ delta_ell = 300
 
 nbins_tomo = 4
 
-kappa_dir = 'data/null_tests/'
-gamma_dir = 'data/des/DESY3_blind_cat_e1e2_maps/'
+kappa_dir = os.path.join(data_dir, 'data/null_tests/')
+# gamma_dir = os.path.join(data_dir, 'data/des/DESY3_blind_cat_e1e2_maps/')
 
-kappa_map_name = 'coadd_4splitlensingmapMV.fits'
-kappa_mask_name = 'act_mask_20220316_GAL060_rms_70.00_d2sk.fits'
+kappa_60_map_names = [
+                        'coadd_4splitlensingmapMV.fits',
+                        'coadd_4splitlensingmapMVPOL.fits',
+                        'coadd_4splitlensingmapnomaskTT.fits',
+                        'coadd_4splitlensingmapTT.fits',
+                        'coaddc_4splitlensingmapMV.fits',
+                        'kappa_MV_alms.fits',
+                        'kappa_MVPOL_alms.fits',
+                        # 'kappa_TT_alms.fit',
+                    ]
+
+# kappa_map_name = 'coadd_4splitlensingmapMV.fits'
+# kappa_mask_name = 'act_mask_20220316_GAL060_rms_70.00_d2sk.fits'
 
 g1_map_name = 'blind_DESY3_meansub_Rcorrected_g1_map_binX_nside2048.fits'
 g2_map_name = 'blind_DESY3_meansub_Rcorrected_g2_map_binX_nside2048.fits'
@@ -45,7 +58,7 @@ weight_map_name = 'weight_map_binX_nside2048.fits'
 # f150-f090 MV: f_15090_MVsub
 # f150-f090 TT   f_15090_TTsub
 
-t_maps_read = time.time()
+# t_maps_read = time.time()
 
 ## set up cl binning
 nbins = math.ceil((ell_max - ell_min) / delta_ell) + 1
@@ -69,39 +82,19 @@ for i in np.arange(0, nbins, 1):
     weights[lh_edges[i]:lh_edges[i + 1]] = 1.
 
 print('Getting binning...')
-t_bins_start = time.time()
+# t_bins_start = time.time()
 ell_bins = nmt.NmtBin(nside=nside, bpws=bpws, ells=ells, weights=weights, lmax=ell_max)
-t_bins_end = time.time()
+# t_bins_end = time.time()
 print('Done.')
 
-## set up fields
-print('Getting fields...')
-t_fields_start = time.time()
-
-kappa_field_name = kappa_map_name.rstrip('.fits') + '_' + kappa_mask_name.rstrip('.fits')
-kappa_field_fname = os.path.join(kappa_dir, kappa_field_name + '.pkl')
-
-# load act null test map
-kappa_alms = hp.read_alm(os.path.join(kappa_dir, kappa_map_name)).astype('complex128')
-kappa_map = hp.alm2map(np.nan_to_num(kappa_alms), nside=nside)
-
-# load act mask
-kappa_mask = enmap.read_map(os.path.join(kappa_dir, kappa_mask_name))
-kappa_mask = reproject.map2healpix(kappa_mask,
-                                   nside=nside, lmax=None, out=None, rot=None,
-                                   spin=[0,2], method="spline", order=0,
-                                   extensive=False, bsize=100000,
-                                   nside_mode="pow2", boundary="constant", verbose=False)
-
-kappa_field = nmt.NmtField(kappa_mask, [kappa_map], masked_on_input=True)
-
+## set up gamma field (used for all)
 gamma_field = {}
 
 for ibin in np.arange(1, nbins_tomo + 1):
 
     print(f'DES bin {ibin} field...')
 
-    gamma_mask = hp.read_map(os.path.join(gamma_dir, weight_map_name.replace('binX', f'bin{ibin}')))
+    # gamma_mask = hp.read_map(os.path.join(gamma_dir, weight_map_name.replace('binX', f'bin{ibin}')))
 
     g1_field_name = g1_map_name.replace('binX', f'bin{ibin}').rstrip('.fits')
     g2_field_name = g2_map_name.replace('binX', f'bin{ibin}').rstrip('.fits')
@@ -115,60 +108,86 @@ for ibin in np.arange(1, nbins_tomo + 1):
 
     gamma_field[ibin] = nmt.NmtField(gamma_mask, [g1_map, g2_map], masked_on_input=True)
 
-t_fields_end = time.time()
+
+# load act mask
+kappa_mask = enmap.read_map(os.path.join(kappa_dir, kappa_mask_name))
+kappa_mask = reproject.map2healpix(kappa_mask,
+                                   nside=nside, lmax=None, out=None, rot=None,
+                                   spin=[0,2], method="spline", order=0,
+                                   extensive=False, bsize=100000,
+                                   nside_mode="pow2", boundary="constant", verbose=False)
+
+# t_fields_end = time.time()
 print('Done in {}s.'.format(t_fields_start - t_fields_end))
 
-## set up workspace
-workspace_kappagamma = {}
-## compute coupling matix
-print('Getting coupling matrix...')
-t_coupling_start = time.time()
-for ibin in np.arange(1, nbins_tomo + 1):
+for kappa_map_name in kappa_60_map_names:
 
-    workspace_fname = f'workspace_kappagamma_bin{ibin}.fits'
+    ## set up kappa field 
+    print('Getting fields...')
+    # t_fields_start = time.time()
 
-    if os.path.exists(workspace_fname):
-        workspace_kappagamma[ibin] = nmt.NmtWorkspace()
-        workspace_kappagamma[ibin].read_from(workspace_fname)
-    else:
-        workspace_kappagamma[ibin] = nmt.NmtWorkspace()
-        workspace_kappagamma[ibin].compute_coupling_matrix(kappa_field, gamma_field[ibin], ell_bins)
-        workspace_kappagamma[ibin].write_to(workspace_fname)
-t_coupling_end = time.time()
-print('Done in {}s.'.format(t_coupling_start - t_coupling_end))
+    kappa_field_name = kappa_map_name.rstrip('.fits') + '_' + kappa_mask_name.rstrip('.fits')
+    kappa_field_fname = os.path.join(kappa_dir, kappa_field_name + '.pkl')
 
-cl_coupled = {}
-cl_decoupled = {}
-
-## compute cls
-for ibin in np.arange(1, nbins_tomo + 1):
-
-    print('Getting coupled Cls...')
-    t_coupledcl_start = time.time()
-    cl_coupled[ibin] = nmt.compute_coupled_cell(gamma_field[ibin], kappa_field)
-    t_coupledcl_end = time.time()
-    print('Done in {}s.'.format(t_coupledcl_start - t_coupledcl_end))
-
-    print('Getting decoupled Cls...')
-    t_decoupledcl_start = time.time()
-    cl_decoupled[ibin] = workspace_kappagamma[ibin].decouple_cell(cl_coupled[ibin])[0]
-    t_decoupledcl_end = time.time()
-    print('Done in {}s.'.format(t_decoupledcl_start - t_decoupledcl_end))
-
-plt.close('all')
-plt.figure(1, figsize=(4.5, 3.75))
-for ibin in np.arange(1, nbins_tomo + 1):
-    plt.plot(ell_bins.get_effective_ells(), cl_decoupled[ibin], '.-')
-
-ax = plt.gca()
-plt.xlim([0,ells.max()])
-plt.ylim([-5.e-12, 5.e-12])
-
-ax.fill_between(ells, ax.get_ylim()[0], ax.get_ylim()[1], where=(ells > 1900), color='silver', alpha=0.5)
-ax.fill_between(ells, ax.get_ylim()[0], ax.get_ylim()[1], where=(ells < 100), color='silver', alpha=0.5)
+    # load act null test map
+    kappa_alms = hp.read_alm(os.path.join(kappa_dir, kappa_map_name)).astype('complex128')
+    kappa_map = hp.alm2map(np.nan_to_num(kappa_alms), nside=nside)
 
 
-plt.axhline(0.0, color='k')#, linestyle='dashed', alpha=0.4)
-plt.xlabel('$\ell$')
-plt.ylabel('$\Delta C^{XY}_{\ell}$')
-plt.savefig('figures/example_null.png', dpi=300, bbox_inches='tight')
+    kappa_field = nmt.NmtField(kappa_mask, [kappa_map], masked_on_input=True)
+
+    ## set up workspace
+    workspace_kappagamma = {}
+    ## compute coupling matix
+    print('Getting coupling matrix...')
+    # t_coupling_start = time.time()
+    for ibin in np.arange(1, nbins_tomo + 1):
+
+        workspace_fname = f'workspace_{kappa_map_name}_kappagamma_bin{ibin}.fits'
+
+        if os.path.exists(workspace_fname):
+            workspace_kappagamma[ibin] = nmt.NmtWorkspace()
+            workspace_kappagamma[ibin].read_from(workspace_fname)
+        else:
+            workspace_kappagamma[ibin] = nmt.NmtWorkspace()
+            workspace_kappagamma[ibin].compute_coupling_matrix(kappa_field, gamma_field[ibin], ell_bins)
+            workspace_kappagamma[ibin].write_to(workspace_fname)
+    # t_coupling_end = time.time()
+    print('Done in {}s.'.format(t_coupling_start - t_coupling_end))
+
+    cl_coupled = {}
+    cl_decoupled = {}
+
+    ## compute cls
+    for ibin in np.arange(1, nbins_tomo + 1):
+
+        print('Getting coupled Cls...')
+        # t_coupledcl_start = time.time()
+        cl_coupled[ibin] = nmt.compute_coupled_cell(gamma_field[ibin], kappa_field)
+        # t_coupledcl_end = time.time()
+        print('Done in {}s.'.format(t_coupledcl_start - t_coupledcl_end))
+
+        print('Getting decoupled Cls...')
+        # t_decoupledcl_start = time.time()
+        cl_decoupled[ibin] = workspace_kappagamma[ibin].decouple_cell(cl_coupled[ibin])[0]
+        # t_decoupledcl_end = time.time()
+        print('Done in {}s.'.format(t_decoupledcl_start - t_decoupledcl_end))
+
+    plt.close('all')
+    plt.figure(1, figsize=(4.5, 3.75))
+    for ibin in np.arange(1, nbins_tomo + 1):
+        plt.plot(ell_bins.get_effective_ells(), cl_decoupled[ibin], '.-')
+
+    ax = plt.gca()
+    plt.xlim([0,ells.max()])
+    plt.ylim([-5.e-12, 5.e-12])
+
+    ax.fill_between(ells, ax.get_ylim()[0], ax.get_ylim()[1], where=(ells > 1900), color='silver', alpha=0.5)
+    ax.fill_between(ells, ax.get_ylim()[0], ax.get_ylim()[1], where=(ells < 100), color='silver', alpha=0.5)
+
+    np.savetxt(f'./output/{kappa_map_name}_ellcl.dat', np.column_stack([ell_bins.get_effective_ells(), cl_decoupled])
+
+    plt.axhline(0.0, color='k')#, linestyle='dashed', alpha=0.4)
+    plt.xlabel('$\ell$')
+    plt.ylabel('$\Delta C^{XY}_{\ell}$')
+    plt.savefig(f'figures/{kappa_map_name}_null.png', dpi=300, bbox_inches='tight')
